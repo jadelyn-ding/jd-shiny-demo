4ce1d746c06e7be659ea388d2c09a77b98cdf9aa
Jadelyn Ding <jld118@duke.edu>
2024-04-23 12:44:39 -0400

    made weather app with mine and prisha :)

--- app.R
+++ app.R
@@ -15,15 +15,46 @@ shinyApp(
     sidebarLayout(
       sidebarPanel(
         # UI input code goes here
+        selectInput(
+          # naming input as city
+          inputId = "city",
+          # user sees label as "Select a city"
+          label = "Select a city",
+          choices = c(
+            "Chicago",
+            "Durham",
+            "New York",
+            "Sedona",
+            "Los Angeles"
+          ),
+          selected = "Durham"
+        ),
+        selectInput(
+          inputId = "variable",
+          label = "Select a variable",
+          choices = c(
+            "temp",
+            "feelslike",
+            "humidity"
+          ),
+          selected = "temp"
+        )
       ),
       mainPanel(
         # UI output code goes here
+        plotOutput("plot")
       )
     )
   ),
   server = function(input, output, session) {
 
     # server code goes here
+    output$plot <- renderPlot({
+      weather |>
+        filter(city == input$city) |>
+        ggplot(aes(x = time, y = input$variable)) +
+        geom_line()
+    })
 
   }
 )
